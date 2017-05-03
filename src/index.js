c51a24fbfd9c26a7f428cd0392e919101099a238
addykumar27 <adarshna27@yahoo.com>
2017-05-03 12:31:02 -0700

    getting HelloWorld to render on the page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 // Developer TODO: Load in App component and render to the DOM
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js';
+
+console.log('indexpage is connected');
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
 
