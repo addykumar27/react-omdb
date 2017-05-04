6608a3359030c7eff2b0bda4ca049110829e1616
addykumar27 <adarshna27@yahoo.com>
2017-05-04 16:08:36 -0700

    adding results, search and utils files and codes

--- src/Home.js
+++ src/Home.js
@@ -4,18 +4,16 @@ import Search from './Search.js'
 import styles from './styles/index'
 
 class Home extends Component {
-	render() {
-
-		return (
-			<div style={styles.transparentBg} className="jumbotron col-sm-12 text-center">
-			    <h1><a href="/">Team 3</a></h1>
-			        <div className="col-sm-12" style={styles.space}>
-			      	<Search />
-			       </div>  			
-			     </div>
-		);
-		console.log('Home page is working');
-	}
+  render() {
+    return (
+      <div style={styles.transparentBg} className="jumbotron col-sm-12 text-center">
+        <h1><a href="/">OMDB React</a></h1>
+        <div className="col-sm-12" style={styles.space}>
+          <SearchContainer />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home
--- src/Search.js
+++ src/Search.js
@@ -2,34 +2,30 @@ import React, {Component} from "react"
 import styles from './styles/index'
 
 class Search extends Component {
-	render(){
-	console.log('show button');
-		return (
-			<form>
-		    <div>
-		    	<input type="text" class="inputbox" placeholder="Search Movies"/>
-				<button type="submit" class='addy'>submit</button>
-			</div>
-			</form>
-
-		)
-	}
+  render(){
+    let {handleSearchInput, handleSubmitQuery, query} = this.props
+    return (
+      <div style={styles.transparentBg} className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+        <div className="col-sm-12">
+          <form onSubmit={(evt) => handleSubmitQuery(evt)}>
+            <div className="form-group">
+              <input
+                onChange={(evt) => handleSearchInput(evt)}
+                value={query}
+                className="form-control"
+                type="text"
+                placeholder="Enter a Movie Title..." />
+            </div>
+            <div className="form-group col-sm-4 col-sm-offset-4">
+              <button
+                className="btn btn-block btn-primary"
+                type="submit">Search</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default Search
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default Search
